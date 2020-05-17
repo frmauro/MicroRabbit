c53970377959b1dd84414ea3d3fdee1ce15a6200
francisco <frmauro8@hotmail.com>
2020-05-17 09:43:03 -0300

    Implementation Proxy TransferService

---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,28 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TransferService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:5001/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), 
+                System.Text.Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
